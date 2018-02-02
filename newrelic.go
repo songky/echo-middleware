5f3dba2aec98c7a679a061ff3f06400314d496de
Pedro Neto <pedroneto1990@gmail.com>
2018-02-02 14:47:32 -0200

    Add request method to newrelic transaction name

--- newrelic.go
+++ newrelic.go
@@ -23,7 +23,8 @@ func NewRelic(appName string, licenseKey string) echo.MiddlewareFunc {
 func NewRelicWithApplication(app nr.Application) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			txn := app.StartTransaction(c.Path(), c.Response().Writer, c.Request())
+			transactionName := fmt.Sprintf("%s %s", c.Path(), c.Request().Method)
+			txn := app.StartTransaction(transactionName, c.Response().Writer, c.Request())
 			defer txn.End()
 
 			c.Set("newrelic-txn", txn)
