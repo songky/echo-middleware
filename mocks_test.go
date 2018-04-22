e53a67f88f818ca05607a0a7d510572ae3505d1a
Fabio Ribeiro <fabiorphp@gmail.com>
2018-04-21 22:57:09 -0300

    Logrus Http request logs
    * Add logrus for http requesting logs
    * Improve docs
    * Change the golang versions into travis

--- mocks_test.go
+++ mocks_test.go
@@ -1,13 +1,18 @@
 package middleware
 
 import (
+	"bytes"
+	"github.com/labstack/gommon/log"
 	nr "github.com/newrelic/go-agent"
+	"io"
 	"net/http"
 	"time"
 )
 
 type (
 	Application struct{}
+
+	MockLogger struct{}
 
 	Transaction struct {
 		http.ResponseWriter
@@ -56,4 +61,88 @@ func (t *Transaction) AddAttribute(key string, value interface{}) error {
 
 func (t *Transaction) StartSegmentNow() nr.SegmentStartTime {
 	return nr.SegmentStartTime{}
+}
+
+func (m *MockLogger) Output() io.Writer {
+	return new(bytes.Buffer)
+}
+
+func (m *MockLogger) SetOutput(w io.Writer) {
+}
+
+func (m *MockLogger) Prefix() string {
+	return ""
+}
+
+func (m *MockLogger) SetPrefix(p string) {
+}
+
+func (m *MockLogger) Level() log.Lvl {
+	return log.INFO
+}
+
+func (m *MockLogger) SetLevel(v log.Lvl) {
+}
+
+func (m *MockLogger) Print(i ...interface{}) {
+}
+
+func (m *MockLogger) Printf(format string, args ...interface{}) {
+
+}
+func (m *MockLogger) Printj(j log.JSON) {
+
+}
+func (m *MockLogger) Debug(i ...interface{}) {
+
+}
+func (m *MockLogger) Debugf(format string, args ...interface{}) {
+}
+
+func (m *MockLogger) Debugj(j log.JSON) {
+}
+
+func (m *MockLogger) Info(i ...interface{}) {
+}
+
+func (m *MockLogger) Infof(format string, args ...interface{}) {
+}
+
+func (m *MockLogger) Infoj(j log.JSON) {
+}
+
+func (m *MockLogger) Warn(i ...interface{}) {
+}
+
+func (m *MockLogger) Warnf(format string, args ...interface{}) {
+}
+
+func (m *MockLogger) Warnj(j log.JSON) {
+}
+
+func (m *MockLogger) Error(i ...interface{}) {
+}
+
+func (m *MockLogger) Errorf(format string, args ...interface{}) {
+}
+
+func (m *MockLogger) Errorj(j log.JSON) {
+}
+
+func (m *MockLogger) Fatal(i ...interface{}) {
+}
+
+func (m *MockLogger) Fatalj(j log.JSON) {
+}
+
+func (m *MockLogger) Fatalf(format string, args ...interface{}) {
+}
+
+func (m *MockLogger) Panic(i ...interface{}) {
+}
+
+func (m *MockLogger) Panicj(j log.JSON) {
+}
+
+func (m *MockLogger) Panicf(format string, args ...interface{}) {
 }
